399027cf2342c268c4075e3eadf00c3760c90057
sumanta2 <sumantagorai.21@gmail.com>
2022-06-02 13:39:16 +0530

    2 vdo2 postgres database add

--- myshop/config/database.js
+++ myshop/config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+      defaultConnection:"default",
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost2'),
+          port: env.int('DATABASE_PORT', 5436),
+          database: env('DATABASE_NAME', 'codewithharry'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', '1234'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        },
+      },
+    });
--- myshop/readme.txt
+++ myshop/readme.txt
@@ -1,3 +1,10 @@
 helping playlist link:  https://youtube.com/playlist?list=PLu0W_9lII9ajKKSG5aROCiw9iro5vK-gE   (codeWihHarry)
 
-video 1 (basic strapi tour)
+video 2 how integrate postgres database with strapi 
+
+location where change to integrate postgres database with strapi myshop => config => database.js
+ helping blog https://strapi.io/blog/postgre-sql-and-strapi-setup  here some error occur to solve this make minor change
+
+ npm install pg used  to install package which help strapi to connect postgres database 
+
+ postgresql database Password 1234
